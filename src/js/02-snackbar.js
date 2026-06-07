dd4066bc54ddec23c6963a015538a1f6dabc017f
Ebru Hızlı <delaglace1995@gmail.com>
2026-06-08 01:22:32 +0300

    itsok

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,5 +1,6 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 iziToast.settings({
     timeout: 3000,
     position: "topRight",
@@ -15,4 +16,37 @@ iziToast.settings({
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', onSubmit);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const delay = Number(formData.get("delay"));
+    const state = formData.get("state");
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+        .then((delay) => {
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay} ms.`,
+                position: "topRight",
+            });
+        }).catch((delay) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay} ms.`,
+                position: "topRight",
+            });
+        });
+});
+
